98dbcc60cbee98bb532d709165cc31da3458d3b7
Nkatouzi <Nastaran_katouzian@yahoo.com>
2025-04-07 18:33:21 -0400

    Add new feature

--- VPA2.py
+++ VPA2.py
@@ -31,7 +31,7 @@ def index():
 @app.route("/start", methods=["POST"])
 def start():
     # When the user presses the "Start" button, we return "I LOVE YOU"
-    return "I LOVE YOU"
+    return "I LOVE YOU Farshid"
 
 if __name__ == "__main__":
     app.run(debug=True)
